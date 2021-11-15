f33e36729b8db2c500cc5dd7d62e2bdf6ab6cada
i-saw-sparks <andresmolinaceti@gmail.com>
2021-11-14 18:19:50 -0600

    baseModel | first commit

---
+++ models/baseModel.php
@@ -0,0 +1,69 @@
+<?php
+class EntidadBase{
+    private $table;
+    private $db;
+    private $conectar;
+ 
+    public function __construct($table) {
+        $this->table=(string) $table;
+         
+        require_once 'Conectar.php'; //TODO archivo de conexión
+        $this->conectar=new Conectar();
+        $this->db=$this->conectar->conexion();
+    }
+     
+    public function getConetar(){
+        return $this->conectar;
+    }
+     
+    public function db(){
+        return $this->db;
+    }
+     
+    public function getAll(){
+        $query=$this->db->query("SELECT * FROM $this->table ORDER BY id DESC");
+          
+        while ($row = $query->fetch_object()) {
+           $resultSet[]=$row;
+        }
+         
+        return $resultSet;
+    }
+     
+    public function getBy($column,$value){
+        $query=$this->db->query("SELECT * FROM $this->table WHERE $column='$value'");
+ 
+        while($row = $query->fetch_object()) {
+           $resultSet[]=$row;
+        }
+         
+        return $resultSet;
+    }
+     
+    public function deleteBy($column,$value){
+        $query=$this->db->query("DELETE FROM $this->table WHERE $column='$value'");
+        return $query;
+    }
+     
+    public function ejecutarSql($query){
+        $query=$this->db()->query($query);
+        if($query==true){
+            if($query->num_rows>1){
+                while($row = $query->fetch_object()) {
+                   $resultSet[]=$row;
+                }
+            }elseif($query->num_rows==1){
+                if($row = $query->fetch_object()) {
+                    $resultSet=$row;
+                }
+            }else{
+                $resultSet=true;
+            }
+        }else{
+            $resultSet=false;
+        }
+         
+        return $resultSet;
+    }  
+}
+?>
