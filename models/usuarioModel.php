cb721ec69c9ea60d6ddf67fc45b43fab11729ca4
Andres Molina <andresmolinaceti@gmail.com>
2021-11-29 00:05:32 -0600

    email domain validation on register

--- models/usuarioModel.php
+++ models/usuarioModel.php
@@ -1,22 +1,37 @@
 <?php
-class UsuariosModel extends ModeloBase{
+class UsuariosModel extends ModeloBase
+{
     private $table;
-     
-    public function __construct(){
-        $this->table="usuario";
+
+    public function __construct()
+    {
+        $this->table = "usuario";
         parent::__construct($this->table);
+    }
+
+    private function endsWith($haystack, $needle)
+    {
+        $length = strlen($needle);
+        if (!$length) {
+            return true;
+        }
+        return substr($haystack, -$length) === $needle;
     }
 
     public function addUser($nombre, $correo, $contrasenia, $contrasenia_rep)
     {
-        if($contrasenia != $contrasenia_rep){
+        if ($contrasenia != $contrasenia_rep) {
             return false;
         }
 
-        $contrasenia = password_hash($contrasenia, PASSWORD_BCRYPT, ['cost'=>4]);
+        if ($this->endsWith($correo, '@alumnos.udg.mx')) {
+            $contrasenia = password_hash($contrasenia, PASSWORD_BCRYPT, ['cost' => 4]);
 
-        $query = $this->db->query("INSERT usuario (nombre, correo, contrasenia) VALUES ('$nombre', '$correo', '$contrasenia')");
+            $query = $this->db->query("INSERT usuario (nombre, correo, contrasenia) VALUES ('$nombre', '$correo', '$contrasenia')");
 
-        return $query;
+            return $query;
+        } else {
+            return false;
+        }
     }
 }
